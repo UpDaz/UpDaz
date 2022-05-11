97ce5bb7d99f13061bf84be757a4a8d061a7df2b
Matthieu <matthieu.dazord@gmail.com>
2022-05-11 23:08:37 +0200

    home page + logo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="h-screen w-screen flex items-center justify-center bg-blue">
+        <img src="{{ asset('img/logo.svg') }}" class="w-6/12 h-50" alt="UpDaz Logo">
+    </div>
+@endsection
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        'blue' : '#1940FF',
+        'blue-dark' : '#001A9E',
+        'brown' : '#9E7700',
+        'orange' : '#EBB100'
+      },
+      backgroundColor : {
+        'blue' : '#1940FF',
+        'blue-dark' : '#001A9E',
+        'brown' : '#9E7700',
+        'orange' : '#EBB100'
+      },
+    },
+  },
+  plugins: [],
+}

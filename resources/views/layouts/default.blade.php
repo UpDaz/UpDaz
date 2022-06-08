4de02b7b9c501d32b460bc5228f9b79804cf86a3
Matthieu <matthieu.dazord@gmail.com>
2022-06-08 16:04:09 +0200

    force cache css

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -21,7 +21,7 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@700&display=swap" rel="stylesheet" defer async>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" defer async>
-    <link rel="stylesheet" href="{{ asset("css/app.css") }}" defer async>
+    <link rel="stylesheet" href="{{ asset("css/app.css") }}?v={{ time() }}" defer async>
     <!-- FOR IE9 below -->
     <!--[if lt IE 9]>
         <script src="js/respond.min.js"></script>

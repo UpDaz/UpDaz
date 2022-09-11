66e86d33ede5539a47a98e35f1f50317d3b93e93
Matthieu <matthieu.dazord@gmail.com>
2022-09-11 12:09:20 +0200

    add canonical url

---
+++ app/View/Components/CanonicalUrl.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\View\Component;
+
+class CanonicalUrl extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.canonical-url', [
+            'canonicalUrl' => $this->getCanonicalUrl(),
+        ]);
+    }
+
+    private function getCanonicalUrl()
+    {
+        $currentUrl = URL::current();
+        return str_replace([
+            'http://',
+            'www.'
+        ],
+        [
+            'https://',
+            '',
+        ],
+        $currentUrl);
+    }
+}

657a0cf1fd0143f98bad31d5ee66af5d59dfc7c4
Matthieu <matthieu.dazord@gmail.com>
2022-09-22 12:34:04 +0200

    remove unused elements

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,6 @@
     @include('elements.presentation')
     @include('elements.references')
     @include('elements.pricing')
-    {{-- @include('elements.your-project') --}}
-    {{-- @include('elements.services') --}}
     <div class="bg-white px-8 md:px-16 py-16 md:py-24">
         @include('elements.contact')
     </div>

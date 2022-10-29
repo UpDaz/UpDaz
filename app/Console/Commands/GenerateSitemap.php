22c277d785c52ba8edbfc9ac4462d2f1b44199b4
Matthieu <matthieu.dazord@gmail.com>
2022-10-29 15:09:19 +0200

    custom generation sitemap

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Tags\Url;
 
 class GenerateSitemap extends Command
 {
@@ -30,6 +31,18 @@ class GenerateSitemap extends Command
     {
         // modify this to your own needs
         SitemapGenerator::create(config('app.url'))
+            ->hasCrawled(function (Url $url) {
+                if(substr($url->path(), -1, 1) == '/') {
+                    return;
+                }
+                if ($url->segment(1) === 'actualites') {
+                    $url->setPriority(0.9);
+                } else {
+                    $url->setPriority(1);
+                }
+                $url->setChangeFrequency(URL::CHANGE_FREQUENCY_NEVER);
+                return $url;
+            })
             ->writeToFile(public_path('sitemap.xml'));
     }
 }

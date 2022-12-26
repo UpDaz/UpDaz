a194fd32b8935db658a5a7ffdfb15e39594f3008
Matthieu <matthieu.dazord@gmail.com>
2022-12-26 10:45:51 +0100

    update composer && resolve error article structured data

--- resources/views/elements/article/structured-data.blade.php
+++ resources/views/elements/article/structured-data.blade.php
@@ -4,7 +4,6 @@
     "@type": "NewsArticle",
     "headline": "{{ $article->title }}",
     "datePublished": "{{ $article->published_at->format('Y-m-d') }}T08:00:00+01:00",
-    "articleBody": "{{ $article->content }}",
     "articleSection": "{{ $article->category->name }}",
     "author": {
         "@type": "Person",

c4513a020c3d968e079fc0eb1af4d76fef49ead7
Matthieu <matthieu.dazord@gmail.com>
2024-07-30 14:55:54 +0200

    update references

--- resources/views/elements/home/references-updaz.blade.php
+++ resources/views/elements/home/references-updaz.blade.php
@@ -1,106 +1,254 @@
-<div class="w-full p-4 lg:w-1/4 md:w-1/2">
-    <div class="items-center justify-start block h-full gap-4 p-6 rounded-lg bg-gradient-to-br from-blue-dark to-blue">
-        <a href="https://editionsvetiver.com/" target="_blank" title="Éditions Vetiver">
-            <img src="{{ asset('img/references/editions-vetiver.svg') }}" class="w-full mx-auto max-h-16" width="138" height="50" alt="Éditions Vetiver logo" title="Éditions Vetiver" loading="lazy"/>
+<x-reference title="PadelReference" :verticaly="true">
+    <x-slot:logo>
+        <img src="{{ asset('img/references/padelreference.svg') }}" width="138" height="30"
+            alt="PadelReference logo" title="Asphodèle PadelReference" loading="lazy" />
+    </x-slot>
+    <x-slot:content>
+        <ul class="pl-3 list-disc">
+            <li>
+                Création d'un site web e-commerce sur-mesure
+            </li>
+            <li>
+                Intégration sur-mesure
+            </li>
+            <li>
+                Optimisations des performances
+            </li>
+            <li>
+                Accompagnement et conseils à la rédaction des besoins
+            </li>
+            <li>
+                Support et évolutions
+            </li>
+        </ul>
+        <a href="https://www.padelreference.com/fr/" target="_blank" title="Asphodèle Créations"
+            class="block px-6 py-3 mt-4 font-medium text-center text-white rounded shadow-md md:inline-block bg-orange hover:bg-blue-dark">
+            Voir le site
+            <x-heroicon-o-arrow-top-right-on-square />
         </a>
-        <div class="mt-4">
-            <h3 class="m-0 text-white">Éditions Vetiver</h3>
-            <h4 class="text-xs text-white font-roboto">Maintenance et évolutions Wordpress / Woocommerce</h4>
-        </div>
-    </div>
-</div>
+    </x-slot>
+</x-reference>
 
-<div class="w-full p-4 lg:w-1/4 md:w-1/2">
-    <div class="items-center justify-start block h-full gap-4 p-6 rounded-lg md:flex bg-gradient-to-br from-blue-dark to-blue">
-        <a href="https://mabeautebio.fr/" target="_blank" title="Ma Beauté Bio" class="block w-1/3 h-auto p-2 mx-auto bg-white md:inline-block">
-            <img src="{{ asset('img/references/ma-beaute-bio.jpeg') }}" class="w-full h-auto " width="30" height="20" alt="Ma Beauté Bio logo" title="Ma Beauté Bio" loading="lazy"/>
+<x-reference title="Mostiglass">
+    <x-slot:logo>
+        <img src="{{ asset('img/references/mostiglass.jpeg') }}" width="138" height="30"
+            alt="Mostiglass logo" title="Mostiglass" loading="lazy" class="p-2 bg-white"/>
+    </x-slot>
+    <x-slot:content>
+        <ul class="pl-3 list-disc">
+            <li>
+                Création d'un site web sur-mesure
+            </li>
+            <li>
+                Intégration sur-mesure sur la base d'une charte graphique
+            </li>
+            <li>
+                Réalisation du tunnel d'aide au choix de produit sur-mesure
+            </li>
+            <li>
+                Rédaction du contenu du site
+            </li>
+            <li>
+                Gestion des intervants
+            </li>
+            <li>
+                Optimisations des performances
+            </li>
+            <li>
+                Accompagnement et conseils à la rédaction des besoins
+            </li>
+            <li>
+                Support et évolutions
+            </li>
+        </ul>
+        <a href="https://www.padelreference.com/fr/" target="_blank" title="Asphodèle Créations"
+            class="block px-6 py-3 mt-4 font-medium text-center text-white rounded shadow-md md:inline-block bg-orange hover:bg-blue-dark">
+            Voir le site
+            <x-heroicon-o-arrow-top-right-on-square />
         </a>
-        <div class="mt-4 md:mt-0">
-            <h3 class="m-0 text-white">Ma beauté Bio</h3>
-            <h4 class="text-xs text-white font-roboto title-font">Maintenance et évolutions Prestashop - Ecommerce</h4>
-        </div>
-    </div>
-</div>
+    </x-slot>
+</x-reference>
 
-<div class="w-full p-4 lg:w-1/4 md:w-1/2">
-    <div class="h-full p-6 rounded-lg bg-gradient-to-br from-blue-dark to-blue">
-        <img src="{{ asset('img/references/candc.svg') }}" class="w-full mx-auto max-h-12" width="138" height="30" alt="C&C Graphic" title="CAndC Agence de communication visuelle Paris" loading="lazy"/>
-        <div class="mt-4">
-            <h3 class="m-0 text-white">C&C Graphic</h3>
-            <h4 class="text-xs text-white font-roboto title-font">Accompagnement - expertise technique</h4>
-        </div>
-    </div>
-</div>
-
-<div class="w-full p-4 lg:w-1/4 md:w-1/2">
-    <div class="items-center justify-start h-full gap-4 p-6 rounded-lg md:flex bg-gradient-to-br from-blue-dark to-blue">
-        <a href="https://www.le5eme.com/fr" target="_blank" title="Le5eme" class="w-1/2 mx-auto">
-            <img src="{{ asset('img/references/le5eme.svg') }}" class="w-full mx-auto max-h-16" width="138" height="50" alt="Le5eme" title="Le5eme.com" loading="lazy"/>
+<x-reference title="Asphodèle Créations" :verticaly="true">
+    <x-slot:logo>
+        <img src="{{ asset('img/references/asphodele-creations.svg') }}" width="138" height="30"
+            alt="Asphodèle Créations logo" title="Asphodèle Créations" loading="lazy" />
+    </x-slot>
+    <x-slot:content>
+        <ul class="pl-3 list-disc">
+            <li>
+                Création d'un site web sur la technologie Webflow
+            </li>
+            <li>
+                Accompagnement et conseils
+            </li>
+            <li>
+                Support et évolutions
+            </li>
+        </ul>
+        <a href="https://www.asphodele-creations.fr/" target="_blank" title="Asphodèle Créations"
+            class="block px-6 py-3 mt-4 font-medium text-center text-white rounded shadow-md md:inline-block bg-orange hover:bg-blue-dark">
+            Voir le site
+            <x-heroicon-o-arrow-top-right-on-square />
         </a>
-        <div class="mt-4 md:mt-0">
-            <h3 class="m-0 text-white">Le5eme</h3>
-            <h4 class="text-xs text-white font-roboto title-font">Création site Webflow</h4>
-        </div>
-    </div>
-</div>
+    </x-slot>
+</x-reference>
 
-<div class="w-full p-4 lg:w-1/4 md:w-1/2">
-    <div class="h-full p-6 rounded-lg bg-gradient-to-br from-blue-dark to-blue">
-        <div class="p-2 bg-white">
-            <img src="{{ asset('img/references/mediaffiliation.png') }}" class="w-full mx-auto max-h-12" width="138" height="30" alt="C" title="Mediaffiliation" loading="lazy"/>
-        </div>
-        <div class="mt-4">
-            <h3 class="m-0 text-white">Mediaffiliation</h3>
-            <h4 class="text-xs text-white font-roboto title-font">Accompagnement - expertise technique</h4>
-        </div>
-    </div>
-</div>
-
-<div class="w-full p-4 lg:w-1/4 md:w-1/2">
-    <div class="h-full p-6 rounded-lg bg-gradient-to-br from-blue-dark to-blue">
-        <div class="p-2 bg-white">
-            <img src="{{ asset('img/references/gpbl.png') }}" class="w-auto mx-auto max-h-12" width="138" height="30" alt="GPBL logo" title="Mediaffiliation" loading="lazy"/>
-        </div>
-        <div class="mt-4">
-            <h3 class="m-0 text-white">GPBL Consulting</h3>
-            <h4 class="text-xs text-white font-roboto title-font">Évolution sur-mure natif PHP</h4>
-        </div>
-    </div>
-</div>
-
-
-<div class="w-full p-4 lg:w-1/4 md:w-1/2">
-    <div class="h-full p-6 rounded-lg bg-gradient-to-br from-blue-dark to-blue">
-        <div class="p-2 bg-white">
-            <img src="{{ asset('img/references/fruiteo.png') }}" class="w-full h-auto mx-auto md:w-1/2" width="138" height="30" alt="Agence Fruiteo logo" title="Fruiteo" loading="lazy"/>
-        </div>
-        <div class="mt-4">
-            <h3 class="m-0 text-white">Agence Fruiteo</h3>
-            <h4 class="text-xs text-white font-roboto title-font">Accompagnement - expertise technique</h4>
-        </div>
-    </div>
-</div>
-
-<div class="w-full p-4 lg:w-1/4 md:w-1/2">
-    <div class="h-full p-6 rounded-lg bg-gradient-to-br from-blue-dark to-blue">
-        <a href="https://www.asphodele-creations.fr/" target="_blank" title="Asphodèle Créations">
-            <img src="{{ asset('img/references/asphodele-creations.svg') }}" class="w-auto mx-auto" width="138" height="30" alt="Asphodèle Créations logo" title="Asphodèle Créations" loading="lazy"/>
+<x-reference title="Éditions Vetiver" :verticaly="true">
+    <x-slot:logo>
+        <img src="{{ asset('img/references/editions-vetiver.svg') }}" class="w-full mx-auto max-h-16" width="138"
+            height="50" alt="Éditions Vetiver logo" title="Éditions Vetiver" loading="lazy" />
+    </x-slot>
+    <x-slot:content>
+        <ul class="pl-3 list-disc">
+            <li>
+                Maintenance de Wordpress et de Woocommerce
+            </li>
+            <li>
+                Correction de bugs
+            </li>
+            <li>
+                Conseils et accompagnement
+            </li>
+        </ul>
+        <a href="https://editionsvetiver.com/" target="_blank" title="Éditions Vetiver"
+            class="block px-6 py-3 mt-4 font-medium text-center text-white rounded shadow-md md:inline-block bg-orange hover:bg-blue-dark">
+            Voir le site
+            <x-heroicon-o-arrow-top-right-on-square />
         </a>
-        <div class="mt-4">
-            <h3 class="m-0 text-white">Asphodèle Créations</h3>
-            <h4 class="text-xs text-white font-roboto title-font">Création site Webflow </h4>
-        </div>
-    </div>
-</div>
+    </x-slot>
+</x-reference>
 
-<div class="w-full p-4 lg:w-1/4 md:w-1/2">
-    <div class="h-full p-6 rounded-lg bg-gradient-to-br from-blue-dark to-blue">
-        <a href="https://www.petitpaume.com/" target="_blank" title="Le Petit Paumé" class="inline-block p-2 bg-white">
-            <img src="{{ asset('img/references/le-petit-paume.webp') }}" class="w-auto mx-auto" width="138" height="30" alt="Le petit paumé logo" title="Le Petit Paumé" loading="lazy"/>
+<x-reference title="Ma Beauté Bio">
+    <x-slot:logo>
+        <img src="{{ asset('img/references/ma-beaute-bio.jpeg') }}" class="w-full h-auto p-2 bg-white" width="30"
+                height="20" alt="Ma Beauté Bio logo" title="Ma Beauté Bio" loading="lazy" />
+    </x-slot>
+    <x-slot:content>
+        <ul class="pl-3 list-disc">
+            <li>
+                Maintenance de Prestashop
+            </li>
+            <li>
+                Correction de bugs
+            </li>
+            <li>
+                Conseils et accompagnement
+            </li>
+        </ul>
+        <a href="https://mabeautebio.fr/" target="_blank" title="Éditions Vetiver"
+            class="block px-6 py-3 mt-4 font-medium text-center text-white rounded shadow-md md:inline-block bg-orange hover:bg-blue-dark">
+            Voir le site
+            <x-heroicon-o-arrow-top-right-on-square />
         </a>
-        <div class="mt-4">
-            <h3 class="m-0 text-white">Le Petit Paumé</h3>
-            <h4 class="text-xs text-white font-roboto title-font">Évolutions techniques - Webflow</h4>
-        </div>
-    </div>
-</div>
+    </x-slot>
+</x-reference>
+
+<x-reference title="C&C Graphic" :verticaly="true">
+    <x-slot:logo>
+        <img src="{{ asset('img/references/candc.svg') }}" class="w-full mx-auto max-h-12" width="138" height="30"
+            alt="C&C Graphic" title="CAndC Agence de communication visuelle Paris" loading="lazy" />
+    </x-slot>
+    <x-slot:content>
+        <ul class="pl-3 list-disc">
+            <li>
+                Évolutions sur des plateformes sur-mesure
+            </li>
+            <li>
+                Accompagnement - expertise technique
+            </li>
+        </ul>
+    </x-slot>
+</x-reference>
+
+<x-reference title="Le5eme">
+    <x-slot:logo>
+        <img src="{{ asset('img/references/le5eme.svg') }}" class="w-full mx-auto max-h-16" width="138"
+            height="50" alt="Le5eme" title="Le5eme.com" loading="lazy" />
+    </x-slot>
+    <x-slot:content>
+        <ul class="pl-3 list-disc">
+            <li>
+                Création d'un site sur la technologie Webflow
+            </li>
+            <li>
+                Installation et personnalisation d'un thème
+            </li>
+        </ul>
+        <a href="https://www.le5eme.com/fr" target="_blank" title="Éditions Vetiver"
+            class="block px-6 py-3 mt-4 font-medium text-center text-white rounded shadow-md md:inline-block bg-orange hover:bg-blue-dark">
+            Voir le site
+            <x-heroicon-o-arrow-top-right-on-square />
+        </a>
+    </x-slot>
+</x-reference>
+
+<x-reference title="Mediaffiliation">
+    <x-slot:logo>
+            <img src="{{ asset('img/references/mediaffiliation.png') }}" class="p-2 bg-white" width="138"
+                height="30" alt="C" title="Mediaffiliation" loading="lazy" />
+    </x-slot>
+    <x-slot:content>
+        <ul class="pl-3 list-disc">
+            <li>
+                Accompagnement et expertise technique multi-sites
+            </li>
+            <li>
+                Support et intervention pour correction de bugs
+            </li>
+        </ul>
+    </x-slot>
+</x-reference>
+
+<x-reference title="GPBL Consulting">
+    <x-slot:logo>
+        <img src="{{ asset('img/references/gpbl.png') }}" class="p-2 bg-white" width="138" height="30"
+            alt="GPBL logo" title="Mediaffiliation" loading="lazy" />
+    </x-slot>
+    <x-slot:content>
+        <ul class="pl-3 list-disc">
+            <li>
+                Évolution et support technique sur un site sur-mesure natif en PHP
+            </li>
+        </ul>
+    </x-slot>
+</x-reference>
+
+<x-reference title="Agence Fruiteo">
+    <x-slot:logo>
+        <img src="{{ asset('img/references/fruiteo.png') }}" class="p-2 bg-white" width="138" height="30"
+            alt="Agence Fruiteo logo" title="Fruiteo" loading="lazy" />
+    </x-slot>
+    <x-slot:content>
+        <ul class="pl-3 list-disc">
+            <li>
+                Accompagnement et conseils multi-sites
+            </li>
+        </ul>
+    </x-slot>
+</x-reference>
+
+<x-reference title="Le Petit Paumé">
+    <x-slot:logo>
+        <img src="{{ asset('img/references/le-petit-paume.webp') }}" class="w-auto p-2 mx-auto bg-white" width="138"
+            height="30" alt="Le petit paumé logo" title="Le Petit Paumé" loading="lazy" />
+    </x-slot>
+    <x-slot:content>
+        <ul class="pl-3 list-disc">
+            <li>
+                Évolutions techniques sur un site Webflow déjà existant
+            </li>
+            <li>
+                Accompagnement et conseils
+            </li>
+            <li>
+                Support et évolutions
+            </li>
+        </ul>
+        <a href="https://www.petitpaume.com/" target="_blank" title="Asphodèle Créations"
+            class="block px-6 py-3 mt-4 font-medium text-center text-white rounded shadow-md md:inline-block bg-orange hover:bg-blue-dark">
+            Voir le site
+            <x-heroicon-o-arrow-top-right-on-square />
+        </a>
+    </x-slot>
+</x-reference>
--- resources/views/elements/home/references.blade.php
+++ resources/views/elements/home/references.blade.php
@@ -1,12 +1,12 @@
 <section id="references" class="px-8 py-4 border-gray-200 md:px-16 md:py-16 sm:border-t">
     <div class="container mx-auto">
-      <div class="flex flex-wrap w-full md:mb-10">
+      <div class="flex flex-wrap w-full lg:mb-10">
         <div class="w-full mb-6 lg:w-1/2 lg:mb-0">
           <h2 class="mb-2 text-2xl font-medium text-gray-900 sm:text-3xl title-font">Ils m'ont déjà fait confiance</h2>
           <div class="w-20 h-1 rounded bg-blue"></div>
         </div>
       </div>
-      <div class="flex flex-wrap -m-4">
+      <div class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         @include('elements.home.references-updaz')
       </div>
     </div>

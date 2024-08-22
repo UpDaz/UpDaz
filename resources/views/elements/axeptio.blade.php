c9cc601af27d943865d60f7f5880b3e88de99ddf
Matthieu <matthieu.dazord@gmail.com>
2024-08-22 21:40:04 +0200

    manage analytics

--- resources/views/elements/axeptio.blade.php
+++ resources/views/elements/axeptio.blade.php
@@ -1,25 +1,25 @@
 <script defer>
-    // window.axeptioSettings = {
-    //     clientId: "63fa545408385a13fb80fb00",
-    //     cookiesVersion: "updaz-fr",
-    // };
+    window.axeptioSettings = {
+        clientId: "63fa545408385a13fb80fb00",
+        cookiesVersion: "updaz-fr",
+    };
 
-    // (function(d, s) {
-    //     var t = d.getElementsByTagName(s)[0],
-    //         e = d.createElement(s);
-    //     e.async = true;
-    //     e.src = "//static.axept.io/sdk.js";
-    //     t.parentNode.insertBefore(e, t);
-    // })(document, "script");
+    (function(d, s) {
+        var t = d.getElementsByTagName(s)[0],
+            e = d.createElement(s);
+        e.async = true;
+        e.src = "//static.axept.io/sdk.js";
+        t.parentNode.insertBefore(e, t);
+    })(document, "script");
 
-    // void 0 === window._axcb && (window._axcb = []);
-    // window._axcb.push(function(axeptio) {
-    //     axeptio.on("cookies:complete", function(choices) {
-    //         if (choices.google_analytics) {
-    //             loadGoogleAnalyticsTag()
-    //         }
-    //     });
-    // });
+    void 0 === window._axcb && (window._axcb = []);
+    window._axcb.push(function(axeptio) {
+        axeptio.on("cookies:complete", function(choices) {
+            if (choices.google_analytics) {
+                // loadGoogleAnalyticsTag()
+            }
+        });
+    });
     window.addEventListener("load", function() {
         loadGoogleAnalyticsTag();
     });
